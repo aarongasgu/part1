2ed0fe7830ffe20acba3a24ececd0ef5dd509774
aarongasgu <aarongasgu@gmail.com>
2021-04-04 04:58:12 -0300

    1.11

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -1,7 +1,22 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Statistics = ({title}) => <h1>{title}</h1>
+const Statistics = (props) => {
+  console.log(props)
+  if(props.total === 0 && props.vofi === "vofi"){
+    return <h1>NO HAY ESTADISTICAS</h1>
+  }
+  if(props.total === 0){
+    return <tr><td></td></tr>
+  }
+  return <tr><td>{props.texto} {props.valor}</td></tr>
+
+}
+
+const Button = ({ handleClick, text }) =>
+  <button onClick={handleClick}>
+    {text}
+  </button>
 
 const App = () => {
   // save clicks of each button to its own state
@@ -10,35 +25,42 @@ const App = () => {
   const [bad, setBad] = useState(0)
 
   const comentario = (comment) => () => {
-    if(comment === "good"){
+    if (comment === "good") {
       setGood(good + 1)
-    } else if(comment === "neutral"){
+    } else if (comment === "neutral") {
       setNeutral(neutral + 1)
-    } else if(comment === "bad"){
+    } else if (comment === "bad") {
       setBad(bad + 1)
     }
   }
 
-  let total = (good + neutral + bad)
-  console.log(total)
+  const total = (good + neutral + bad)
+  const promedio = ((good - bad) / total)
+  const positivos = (good * 100 / total + "%")
 
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={comentario("good")}>good</button>
+      <Button handleClick={comentario("good")} text="good" />
+      <Button handleClick={comentario("neutral")} text="neutral" />
+      <Button handleClick={comentario("bad")} text="bad" />
+      {/* <button onClick={comentario("good")}>good</button>
       <button onClick={comentario("neutral")}>neutral</button>
-      <button onClick={comentario("bad")}>bad</button>
-      <Statistics title="Statistics"/>
-      good: {good} <br/>
-      neutral: {neutral} <br/> 
-      bad: {bad} <br/> 
-      total: {total} <br/> 
-      promedio: {total / 3} <br/> 
-      positivos: {good * 100 / total + "%"}
+      <button onClick={comentario("bad")}>bad</button> */}
+      <h1>Statistics</h1>
+      <Statistics vofi="vofi" total={total} />
+      <table><tbody>
+        <Statistics texto="good" valor={good} total={total}/>
+        <Statistics texto="neutral" valor={neutral} total={total}/>
+        <Statistics texto="bad" valor={bad} total={total}/>
+        <Statistics texto="total" valor={total} total={total}/>
+        <Statistics texto="promedio" valor={promedio} total={total}/>
+        <Statistics texto="positivos" valor={positivos} total={total}/>
+      </tbody></table>
     </div>
   )
 }
 
-ReactDOM.render(<App />, 
+ReactDOM.render(<App />,
   document.getElementById('root')
 )
